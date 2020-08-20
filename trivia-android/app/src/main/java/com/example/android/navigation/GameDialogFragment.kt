64952899b60424a31445fff4015d8dfe0610a33e
jules <julia.djanic@stud.fh-campuswien.ac.at>
2020-08-20 20:40:23 +0200

    rename  file names

--- trivia-android/app/src/main/java/com/example/android/navigation/GameDialogFragment.kt
+++ trivia-android/app/src/main/java/com/example/android/navigation/GameDialogFragment.kt
@@ -1,17 +1,14 @@
 package com.example.android.navigation
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 
-class MyDialog : DialogFragment() {
+class GameDialog : DialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_dialog, container, false)
+        return inflater.inflate(R.layout.fragment_game_dialog, container, false)
     }
 }
