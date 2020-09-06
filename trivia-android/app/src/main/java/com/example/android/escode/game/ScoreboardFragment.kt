c403ccae2b1ed04fa6715cfaa4b1b40559cbc7ed
jules <julia.djanic@stud.fh-campuswien.ac.at>
2020-09-06 13:31:16 +0200

    add binding, scoreboard communicator linkage and some layout stuff

--- trivia-android/app/src/main/java/com/example/android/escode/game/ScoreboardFragment.kt
+++ trivia-android/app/src/main/java/com/example/android/escode/game/ScoreboardFragment.kt
@@ -4,12 +4,35 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.android.escode.R
+import com.example.android.escode.databinding.FragmentScoreboardBinding
 
 class ScoreboardFragment : Fragment() {
 
+    var whichLevel: Int? = 0
+    var whichLevelHeartCount: Int? = 0
+    var whichTotalHeartCount: Int? = 0
+
+    lateinit var whichLevelString: String
+    lateinit var whichLevelHeartCountString: String
+    lateinit var whichTotalHeartCountString: String
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_scoreboard, container, false)
+
+        val binding = DataBindingUtil.inflate<FragmentScoreboardBinding>(
+                inflater, R.layout.fragment_scoreboard, container, false)
+
+        whichLevel = arguments?.getInt("indexLevelPass")
+        whichLevelHeartCount = arguments?.getInt("levelHeartPass")
+        whichTotalHeartCount = arguments?.getInt("totalHeartPass")
+
+        whichLevelString = whichLevel.toString()
+        whichLevelHeartCountString = whichLevelHeartCount.toString()
+        whichTotalHeartCountString = whichTotalHeartCount.toString()
+
+        return binding.root
     }
 }
